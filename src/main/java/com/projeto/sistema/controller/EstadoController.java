751852cde9df79ebdcd5a811ac8bd66f902867d2
Riane Bergamo <rrianebergamo@gmail.com>
2024-12-19 16:49:12 -0300

    CRUD implementado

--- src/main/java/com/projeto/sistema/controller/EstadoController.java
+++ src/main/java/com/projeto/sistema/controller/EstadoController.java
@@ -7,9 +7,10 @@ import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
 
 @Controller
 @NoArgsConstructor
@@ -19,12 +20,27 @@ public class EstadoController {
     @Autowired
     private EstadoRepository estadoRepository;
 
-    @GetMapping("/cadastro")//cadastrarEstado
+    @GetMapping("/cadastrar")//cadastrarEstado
     public ModelAndView cadastrar(Estado estado) {
         ModelAndView mv = new ModelAndView("administrativo/estados/cadastro");
         mv.addObject("estado", estado);
 
         return mv;
+    }
+
+    @GetMapping("/listar")//listaEstado
+    public ModelAndView listar() {
+        ModelAndView mv = new ModelAndView("administrativo/estados/lista");
+        mv.addObject("listar", estadoRepository.findAll());
+
+        return mv;
+    }
+
+    @GetMapping("/editar/{id}")//editarEstado
+    public ModelAndView editar(@PathVariable("id") Long id) {
+        Optional<Estado> estado = estadoRepository.findById(id);
+
+        return cadastrar(estado.get());
     }
 
     @PostMapping("/salvar")//salvarEstado
@@ -36,6 +52,15 @@ public class EstadoController {
         estadoRepository.saveAndFlush(estado);
         return cadastrar(new Estado());
     }
+
+    @GetMapping("/remover/{id}")//removerEstado
+    public ModelAndView remover(@PathVariable("id") Long id) {
+        Optional<Estado> estado = estadoRepository.findById(id);
+        estadoRepository.delete(estado.get());
+
+        return listar();
+    }
+
 
 
 
